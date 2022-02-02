cf3bca7b9ce0dfa3b49ed8a79bdb1f5e741c6a06
kiwoon kim <kisoon0947@gmail.com>
2022-02-02 20:50:30 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>hello</div>
+   <div className='bg-red-500'>
+     <h1 className='text-black'>it works</h1>
+   </div>
   )
 }
 
