604da7558d918e457ca8a0039a2286f88f1d8da6
tigerenergy <kisoon0947@gmail.com>
2022-02-09 11:10:41 +0900

    update

---
+++ pages/api/client-test.tsx
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) 
+
+{
+  res.json(
+  {
+    ok: true,
+    data:'shit'
+  })
+}
