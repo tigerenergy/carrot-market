f893ac3d77e47796bd03e234334786c258f05439
tigerenergy <kisoon0947@gmail.com>
2022-02-16 11:27:37 +0900

    update

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -12,7 +12,7 @@ interface LoginForm
 
 export default function Forms()
 {   
-    const { register, handleSubmit} = useForm<LoginForm>()
+    const { register, handleSubmit , formState:{ errors }} = useForm<LoginForm>()
     const onValid = (data: LoginForm) =>
     {
         console.log('im Ok!')
@@ -21,13 +21,18 @@ export default function Forms()
     {
         console.log(errors)
     }
+    console.log(errors)
     return(
         <form onSubmit={handleSubmit(onValid, onInvalid)}>
             <input 
             {...register('username',
                 {
                     required: 'username is required',
-                    
+                    minLength:
+                    {
+                        message: 'The username should be longer than 5 chars.',
+                        value: 5
+                    },
                 }
               )
             }
@@ -38,19 +43,20 @@ export default function Forms()
             <input 
             {...register('email',
                 {
-                    required: 'email is required',
-                    minLength:
-                    {
-                        message: 'The username should be longer than 5 chars.',
-                        value: 5,
-                    },
-                    
+                  required: 'email is required',
+                  validate: 
+                  {
+                      notGmail: (value) => 
+                      !value.includes('@gmail.com') || 'Gmail is not allowed',
+                  }      
                 }
               )
             }
             type='email' 
-            placeholder='Email' 
+            placeholder='Email'
+            className={`${Boolean(errors.email) ? 'border-red-500' : 'border-blue-600'}`}
             />
+            {errors.email?.message}
 
             <input 
             {...register('password',
