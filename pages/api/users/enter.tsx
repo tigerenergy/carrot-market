c76156200e612ac02e2f77712aba793ccfe19953
kiwoon kim <kisoon0947@gmail.com>
2022-02-20 18:32:22 +0900

    update

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,15 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import withHandler from '../../../libs/server/withHandler'
 
-export default async function handler(
+async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) 
 { 
- if(req.method !== 'POST')
- {
-   res.status(401).end()
- }
-
  console.log(req.body)
- res.json({ok: true})
+ res.status(200).end()
 }
+
+export default withHandler('POST' , handler)
