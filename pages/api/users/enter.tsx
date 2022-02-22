638bed349d8b907c4272d70a1f58533182ab7898
tigerenergy <kisoon0947@gmail.com>
2022-02-22 11:07:20 +0900

    update

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,13 +1,32 @@
 import withHandler from '@libs/server/withHandler'
 import { NextApiRequest, NextApiResponse } from 'next'
+import client from '@libs/server/client'
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) 
 { 
- console.log(req.body)
- res.status(200).end()
+ const { phone , email } =req.body
+ const payload = phone ? { phone: +phone } : { email }
+ const user = await client.user.upsert(
+   {
+     where:
+     {
+      ...payload
+     },
+     create:
+     {
+       name:'Anonymous',
+       ...payload
+     },
+     update:
+     {
+
+     }
+   })
+   console.log(user)
+ return res.status(200).end()
 }
 
 export default withHandler('POST' , handler)
