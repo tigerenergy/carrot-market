1ad0f9f31aaede48d86030bd80cf6500ce9965ca
tigerenergy <kisoon0947@gmail.com>
2022-02-25 17:13:05 +0900

    update

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,32 +1,45 @@
-import withHandler from '@libs/server/withHandler'
+import withHandler , { ResponseType } from '@libs/server/withHandler'
 import { NextApiRequest, NextApiResponse } from 'next'
 import client from '@libs/server/client'
 
+
+
 async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<ResponseType>
 ) 
 { 
  const { phone , email } =req.body
- const payload = phone ? { phone: +phone } : { email }
- const user = await client.user.upsert(
+ const user = phone ? { phone: +phone } : email ? { email } : null
+ if(!user) return res.status(400).json({ ok: false})
+ const payload = Math.floor( 100000 + Math.random() * 900000) + ''
+ const token = await client.token.create(
    {
-     where:
+     data:
      {
-      ...payload
+       payload: payload ,
+       user:
+       {
+         connectOrCreate:
+         {
+          where:
+          {
+           ...user
+          },
+          create:
+          {
+            name:'Anonymous',
+            ...user
+          },
+         }
+       }
      },
-     create:
-     {
-       name:'Anonymous',
-       ...payload
-     },
-     update:
-     {
-
-     }
    })
-   console.log(user)
- return res.status(200).end()
+   console.log(token)
+ return res.json(
+   {
+     ok: true
+   })
 }
 
 export default withHandler('POST' , handler)
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './components/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
+    
     extend: {},
   },
   darkMode: 'class',
