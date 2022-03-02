615235c1a380dc2dbeb538517bfc29b36a950f11
tigerenergy <kisoon0947@gmail.com>
2022-03-02 15:35:27 +0900

    update

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,7 +1,10 @@
+import twilio from 'twilio'
 import withHandler , { ResponseType } from '@libs/server/withHandler'
 import { NextApiRequest, NextApiResponse } from 'next'
 import client from '@libs/server/client'
 
+
+const twilioClient = twilio(process.env.TWILIO_SID , process.env.TWILIO_TOKEN)
 
 
 async function handler(
@@ -35,7 +38,29 @@ async function handler(
        }
      },
    })
-   console.log(token)
+   if(phone)
+   {
+    const message = await twilioClient.messages.create(
+       {
+         messagingServiceSid: process.env.TWILIO_MSID,
+         to: process.env.MY_PHONE!,
+         body: `Your login token is ${payload}`,
+       })
+       console.log(message)
+   }
+   if(email)
+   {
+    const message = await twilioClient.messages.create(
+      {
+        messagingServiceSid: process.env.TWILIO_MSID,
+        to: process.env.MY_PHONE!,
+        body: `Your login token is ${payload}`,
+      })
+      console.log(message)
+   }
+ 
+                                               
+   
  return res.json(
    {
      ok: true
