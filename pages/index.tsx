92afcc8d7fdaaae7b125ac804de4b0e5a2a83a8f
tigerenergy <kisoon0947@gmail.com>
2022-03-04 10:35:32 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,20 @@
+import useUser from '@libs/client/useUser'
 import type { NextPage } from 'next'
-import FloatingButton from '../components/floating-button'
-import Item from '../components/item'
-import Layout from '../components/layout'
+import FloatingButton from '@components/floating-button'
+import Item from '@components/item'
+import Layout from '@components/layout'
+import Head from 'next/head'
 
 const Home: NextPage = () => 
-{
+{ 
+  const user = useUser()
+  console.log(user)
+
   return (
     <Layout title='홈' hasTabBar>
+      <Head>
+        <title>Home</title>
+      </Head>
       <div className='flex flex-col space-y-5 divide-y'>
         {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
           <Item
