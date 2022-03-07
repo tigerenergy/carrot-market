8af0a0e82b9a2bb7895ea39942f3763dda5341f4
tigerenergy <kisoon0947@gmail.com>
2022-03-07 15:36:17 +0900

    update

---
+++ pages/api/posts/[id].ts
@@ -0,0 +1,72 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import withHandler , { ResponseType } from '@libs/server/withHandler'
+import { withApiSession } from '@libs/server/withSession'
+import client from '@libs/server/client'
+
+
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) 
+{ 
+ 
+ const { query : { id } , } = req
+ const post = await client.post.findUnique(
+     {
+       where:
+       {
+           id: +id.toString(),
+       },
+       include:
+       {
+           user:
+           {
+               select:
+               {
+                   id: true ,
+                   name: true ,
+                   avatar: true ,
+               },
+            },
+            answers:
+            {
+               select:
+               {
+                   answer: true ,
+                   id: true ,
+                   user:
+                   {
+                        select:
+                        {
+                            id: true ,
+                            name: true ,
+                            avatar: true ,
+                        },
+                   },
+               }, 
+            },
+           _count:
+           {
+               select:
+               {
+                   answers: true ,
+                   wondering : true ,
+               }     
+           }
+       }
+     })
+ res.json(
+     {
+         ok: true,
+         post,
+     })
+}
+
+export default withApiSession(
+    withHandler(
+    {
+        methods: ['GET'],
+        handler,
+    })
+)
