7b46653eb1eeeda6eb91018f177c7da7b6ed666f
tigerenergy <kisoon0947@gmail.com>
2022-03-14 10:50:48 +0900

    update

---
+++ components/product-list.tsx
@@ -0,0 +1,38 @@
+import { ProductWithCount } from 'pages'
+import useSWR from 'swr'
+import Item from './item'
+
+
+interface Record
+{
+  id: number
+  product: ProductWithCount
+}
+
+interface ProductListResponse
+{
+  [key:string]: Record[]
+}
+
+
+interface ProductListProps
+{
+    kind: 'favs' | 'sales' | 'purchases'
+}
+
+export default function ProductList({ kind } : ProductListProps)
+{
+    const { data } = useSWR<ProductListResponse>(`/api/users/me/${kind}`)
+    return data
+        
+        ? <>{data?.sales?.map((record) => (
+        <Item 
+        id={record.product.id} 
+        key={record.id} 
+        title={record.product.name} 
+        price={record.product.price} 
+        hearts={record.product._count.favs}
+        />
+      ))}</>
+      : null
+}
