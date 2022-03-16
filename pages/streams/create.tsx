19f9f1e8c9ddc9ad73c1ba9782465f63bd23b40d
tigerenergy <kisoon0947@gmail.com>
2022-03-16 14:40:51 +0900

    update

---
+++ pages/streams/create.tsx
@@ -0,0 +1,69 @@
+import type { NextPage } from 'next'
+import Button from '@components/button'
+import Input from '@components/input'
+import Layout from '@components/layout'
+import TextArea from '@components/textarea'
+import { useForm } from 'react-hook-form'
+import useMutation  from '@libs/client/useMutation';
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { Stream } from '@prisma/client'
+
+interface CreatForm
+{
+  name: string
+  price: number
+  description: string
+}
+
+interface CreateResponse
+{
+  ok: boolean
+  stream: Stream  
+}
+
+const Create: NextPage = () => 
+{ 
+  const router = useRouter()
+  const [ creatStream , {loading , data } ] = useMutation<CreateResponse>(`/api/streams`)
+  const { register, handleSubmit } = useForm<CreatForm>()
+  const onValid = (form:CreatForm) => 
+  { 
+    if(loading) return
+    creatStream(form)
+  }
+  useEffect(()=>
+  {
+    if(data && data.ok)
+    {
+      router.push(`/streams/${data.stream.id}`)
+    }
+  },[data , ])
+  return (
+    <Layout canGoBack title='Go Live'>
+      <form onSubmit={handleSubmit(onValid)} className=' space-y-4 py-10 px-4'>
+        <Input 
+        register ={register('name' , { required: true })}
+        required 
+        label='Name' 
+        name='name' 
+        type='text' />
+        <Input
+          required
+          register={register('price', {required: true})}
+          label='Price'
+          name='price'
+          type='text'
+          kind='price'
+        />
+        <TextArea
+        register ={register('description' , { required: true })} 
+        name='description' 
+        label='Description' />
+        <Button text={ loading ? 'Loading...' : 'Go live'} />
+      </form>
+    </Layout>
+  )
+}
+
+export default Create
