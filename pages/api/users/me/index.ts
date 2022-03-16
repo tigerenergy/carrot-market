62564830eebcfb1bb26954837f4e0ad5d6f22803
tigerenergy <kisoon0947@gmail.com>
2022-03-16 13:04:11 +0900

    update

--- pages/api/users/me/index.ts
+++ pages/api/users/me/index.ts
@@ -1,114 +1,127 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import withHandler , { ResponseType } from '@libs/server/withHandler'
-import { withApiSession } from '@libs/server/withSession'
+import withHandler, { ResponseType } from '@libs/server/withHandler'
 import client from '@libs/server/client'
-
-
-
+import { withApiSession } from '@libs/server/withSession'
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
-) 
-{ 
- 
-    if(req.method === 'GET')
+) {
+  if (req.method === 'GET') {
+    const profile = await client.user.findUnique(
     {
-        const profile = await client.user.findUnique(
-            {
-                where:
-                {
-                    id: req.session.user?.id
-                },
-            })
-        res.json(
-            {
-                ok: true,
-                profile,
-            })
+      where: { id: req.session.user?.id },
+    })
+    res.json(
+    {
+      ok: true,
+      profile,
+    })
+  }
+  if (req.method === 'POST') {
+    const 
+    {
+      session: { user },
+      body: { email, phone, name },
+    } = req
+    const currentUser = await client.user.findUnique(
+    {
+      where: 
+      {
+        id: user?.id,
+      },
+    })
+    if (email && email !== currentUser?.email) 
+    {
+      const alreadyExists = Boolean(
+        await client.user.findUnique(
+        {
+          where: 
+          {
+            email,
+          },
+          select: 
+          {
+            id: true,
+          },
+        })
+      )
+      if (alreadyExists) 
+      {
+        return res.json(
+        {
+          ok: false,
+          error: 'Email already taken.',
+        })
+      }
+      await client.user.update(
+      {
+        where: 
+        {
+          id: user?.id,
+        },
+        data: 
+        {
+          email,
+        },
+      })
+      res.json({ ok: true })
     }
-    if(req.method === 'POST')
+    if (phone && phone !== currentUser?.phone) 
     {
-        const { session: { user }, body: { email , phone }} = req 
-        if(email)
+      const alreadyExists = Boolean(
+        await client.user.findUnique(
         {
-            const alreadyExists = Boolean(await client.user.findUnique(
-                {
-                    where:
-                    {
-                        email,    
-                    },
-                    select:
-                    {
-                        id: true,
-                    }
-                })
-            )
-            if(alreadyExists)
-            {
-                return res.json(
-                    {
-                        ok: false,
-                        error: 'Email already taken.'
-                    })
-            }
-            await client.user.update(
-                {
-                    where:
-                    {
-                        id: user?.id,
-                    },
-                    data:
-                    {
-                        email,
-                    },       
-                })
-                res.json({ ok: true })
-        }
-        else if(phone)
+          where: 
+          {
+            phone,
+          },
+          select: 
+          {
+            id: true,
+          },
+        })
+      )
+      if (alreadyExists) 
+      {
+        return res.json(
         {
-            if(phone)
-            {
-                const alreadyExists = Boolean(await client.user.findUnique(
-                    {
-                        where:
-                        {
-                            phone,    
-                        },
-                        select:
-                        {
-                            id: true,
-                        }
-                    })
-                )
-                if(alreadyExists)
-                {
-                    return res.json(
-                        {
-                            ok: false,
-                            error: 'Phone already in use.'
-                        })
-                }
-                await client.user.update(
-                    {
-                        where:
-                        {
-                            id: user?.id,
-                        },
-                        data:
-                        {
-                            phone,
-                        },       
-                    })
-                    res.json({ ok: true })
-            }  
-        }
-    } 
+          ok: false,
+          error: 'Phone already in use.',
+        })
+      }
+      await client.user.update({
+        where: 
+        {
+          id: user?.id,
+        },
+        data: 
+        {
+          phone,
+        },
+      })
+      res.json({ ok: true })
+    }
+    if (name) 
+    {
+      await client.user.update(
+    {
+        where: 
+        {
+          id: user?.id,
+        },
+        data: 
+        {
+          name,
+        },
+    })
+    }
+    res.json({ ok: true })
+  }
 }
 
 export default withApiSession(
-    withHandler(
-    {
-        methods: ['GET' , 'POST'],
-        handler,
-    })
+  withHandler({
+    methods: ['GET', 'POST'],
+    handler,
+  })
 )
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -7,55 +7,52 @@ import { useForm } from 'react-hook-form'
 import { useEffect } from 'react'
 import useMutation from '@libs/client/useMutation'
 
-interface EditProfileForm
-{
+interface EditProfileForm {
   email?: string
   phone?: string
+  name?: string
   formErrors?: string
 }
 
-interface EditProfileResponse
-{
+interface EditProfileResponse {
   ok: boolean
   error?: string
 }
 
-const EditProfile: NextPage = () => 
-{ 
+const EditProfile: NextPage = () => {
   const { user } = useUser()
-  const { register , setValue , handleSubmit , setError, formState:{errors} } = useForm<EditProfileForm>()
-
-  useEffect(()=>
-  {
-    if(user?.email)
-    {
-      setValue('email' , user.email)
-    }
-    if(user?.phone)
-    {
-      setValue('phone' , user.phone)
-    }
-  },[user, setValue])
-  const [ editProfile , { data , loading }] = useMutation<EditProfileResponse>(`api/users/me`)
-  const onValid = ({email , phone}:EditProfileForm) =>
-  {
-    if(loading) return
-    if(email === '' && phone === '')
-    {
-      return setError('formErrors' , 
-      { 
-        message:'Email OR phone number are required, You need to choose one.',
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<EditProfileForm>()
+  useEffect(() => {
+    if (user?.name) setValue('name', user.name)
+    if (user?.email) setValue('email', user.email)
+    if (user?.phone) setValue('phone', user.phone)
+  }, [user, setValue])
+  const [editProfile, { data, loading }] =
+    useMutation<EditProfileResponse>(`/api/users/me`)
+  const onValid = ({ email, phone, name }: EditProfileForm) => {
+    if (loading) return
+    if (email === '' && phone === '' && name === '') {
+      return setError('formErrors', {
+        message: 'Email OR Phone number are required. You need to choose one.',
       })
     }
-    editProfile({ email , phone })
+    editProfile({
+      email,
+      phone,
+      name,
+    })
   }
-  useEffect(()=>
-  {
-    if(data && !data.ok && data.error)
-    {
-      setError('formErrors' , {message: data.error})  
+  useEffect(() => {
+    if (data && !data.ok && data.error) {
+      setError('formErrors', { message: data.error })
     }
-  },[ data ])
+  }, [data, setError])
   return (
     <Layout canGoBack title='Edit Profile'>
       <form onSubmit={handleSubmit(onValid)} className='py-10 px-4 space-y-4'>
@@ -74,24 +71,36 @@ const EditProfile: NextPage = () =>
             />
           </label>
         </div>
-        <Input 
-          register ={register('email')}
-          required ={false}
-          label='Email address' 
-          name='email' 
-          type='email' />
         <Input
-          register ={register('phone')}
-          required ={false}
+          register={register('name')}
+          required={false}
+          label='Name'
+          name='name'
+          type='text'
+        />
+        <Input
+          register={register('email')}
+          required={false}
+          label='Email address'
+          name='email'
+          type='email'
+        />
+        <Input
+          register={register('phone')}
+          required={false}
           label='Phone number'
           name='phone'
-          type='number'
-          kind='phone' />
-        {errors.formErrors ? <span className='my-2 text-red-500 font-md block'>{errors.formErrors.message}</span> : null}
-        <Button text={loading ? 'Loading ...' : 'Update profile'}/>
+          type='text'
+          kind='phone'
+        />
+        {errors.formErrors ? (
+          <span className='my-2 text-red-500 font-medium text-center block'>
+            {errors.formErrors.message}
+          </span>
+        ) : null}
+        <Button text={loading ? 'Loading...' : 'Update profile'} />
       </form>
     </Layout>
   )
 }
-
 export default EditProfile
