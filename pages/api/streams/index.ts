019f5105b59c5263fd13a60da122d224a592224e
tigerenergy <kisoon0947@gmail.com>
2022-03-18 14:41:19 +0900

    update

--- pages/api/streams/index.ts
+++ pages/api/streams/index.ts
@@ -34,7 +34,11 @@ async function handler(
         }
     else if(req.method === 'GET')
     {
-        const streams = await client.stream.findMany()
+        const streams = await client.stream.findMany(
+            {
+                take: 10,
+                skip: 10,
+            })
         res.json({ ok: true , streams})
     }
 }
