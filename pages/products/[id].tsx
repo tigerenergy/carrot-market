d909db01a07fe4fb76e470653225519bc597f03d
tigerenergy <kisoon0947@gmail.com>
2022-03-29 11:26:46 +0900

    update

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -38,10 +38,13 @@ const ItemDetail: NextPage = () => {
     <Layout canGoBack>
       <div className='px-4  py-4'>
         <div className='mb-8'>
-          <img
+        <div className="relative pb-80">
+          <Image
             src={`https://imagedelivery.net/iv2WwjdldtCTZq03LtXe7A/${data?.product.image}/public`}
-            className='h-96 bg-slate-300'
+            className='h-96 bg-slate-300 object-cover'
+            layout='fill'
           />
+          </div>
           <div className='flex cursor-pointer py-3 border-t border-b items-center space-x-3'>
             <Image
             width={48}
