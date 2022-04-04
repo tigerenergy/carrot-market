2f7af2a6705d1d3f8ae340b00a1e582581bd4654
tigerenergy <kisoon0947@gmail.com>
2022-04-05 08:59:18 +0900

    update

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -6,7 +6,7 @@ const Chats: NextPage = () =>
   return (
     <Layout hasTabBar title='채팅'>
       <div className='divide-y-[1px] '>
-        {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+        {[1].map((_, i) => (
           <div
             key={i}
             className='flex px-4 cursor-pointer py-3 items-center space-x-3'
---
+++ posts/01-first-post.md
@@ -0,0 +1,5 @@
+# welcome everyone!
+
+This is my first blog post!
+
+Thank you for reading
